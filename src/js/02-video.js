34253364cdd43f370d4327ec1eb2e2a501686247
annnacaesar <Anechka22334@gmail.com>
2022-05-03 21:50:12 +0300

    second task complete without lodash.throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+
+let time = localStorage.getItem('videoplayer-current-time');
+if(time != null) {
+    player.setCurrentTime(time);
+}
+
+player.on('timeupdate', function() {
+    player.getCurrentTime().then(function(seconds) {
+        localStorage.setItem('videoplayer-current-time', seconds);
+    });
+});
