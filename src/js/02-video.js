fe44ef7d4dc7716038d911d7e82eb5973c1cc616
annnacaesar <Anechka22334@gmail.com>
2022-05-05 11:21:16 +0300

    second task with throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,7 @@
 import Player from '@vimeo/player';
 const throttle = require('lodash.throttle');
 
+
 const iframe = document.querySelector('#vimeo-player');
 const player = new Vimeo.Player(iframe);
 
@@ -10,8 +11,12 @@ if(time != null) {
     player.setCurrentTime(time);
 }
 
-player.on('timeupdate', function() {
-    player.getCurrentTime().then(function(seconds) {
+const onCurrentTime = () => {
+    player.getCurrentTime().then(function (seconds) {
         localStorage.setItem('videoplayer-current-time', seconds);
+
     });
-});
+}
+
+player.on('timeupdate', throttle(onCurrentTime, 1000) );
+
